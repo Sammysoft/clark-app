66fa28326110cd61b29c3215e4a1839417044ca2
Samuel Dare-Owonibi <dareowonibisamuel@gmail.com>
2023-09-19 13:49:15 +0000

    some more commits that i have made to make things work

--- src/string.js
+++ src/string.js
@@ -1,2 +1,2 @@
-// export const API = "https://node-clark-api.onrender.com/api"
-export const API = "http://localhost:9099/api"
+export const API = "https://node-clark-api.onrender.com/api"
+// export const API = "http://localhost:9099/api"
